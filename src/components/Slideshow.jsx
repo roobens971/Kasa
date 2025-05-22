6c013b71320d5bad7045620d89920b457e20cb2a
roobens landre <roobens971@gmail.com>
2025-05-22 21:06:25 +0200

    Supprimer les commentaires - Slideshow :fire:

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -10,16 +10,14 @@ function Slideshow({ pictures }) {
   let [activeSlide, setActiveSlide] = useState(0);
   const finalSlides = pictures.length;
 
-// Si on est à la première image, on revient à la dernière image (boucle infini).
-// Sinon, on recule de 1.
+
   const slideLeft = () => {
     setActiveSlide((prevIndex) =>
       prevIndex === 0 ? pictures.length - 1 : prevIndex - 1
     );
   };
 
-// Si on est à la dernière image, on revient à la première image.
-// Sinon, on avance de 1.
+
   const slideRight = () => {
     setActiveSlide((prevIndex) =>
       prevIndex === pictures.length - 1 ? 0 : prevIndex + 1
