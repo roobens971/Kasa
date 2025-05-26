a7b090d75a1474b5f00f752a5c6d41f2050a676d
roobens landre <roobens971@gmail.com>
2025-05-26 16:38:36 +0200

    Supprimer CardItem :fire:

--- src/components/Logements.jsx
+++ src/components/Logements.jsx
@@ -1,14 +1,20 @@
 import data from "../data.js";
 import Card from "./Card.jsx";
 function Logements() {
-  
   return (
-    <div className="DisplayCard">
+    <div className="LogementsContainer">
+      <div className="DisplayCard">
         {data.map((logement) => (
-          <Card key={logement.id} id={logement.id} title={logement.title} img={logement.cover} />
+          <Card
+            key={logement.id}
+            id={logement.id}
+            title={logement.title}
+            img={logement.cover}
+          />
         ))}
+      </div>
     </div>
   );
-}   
+}
 
 export default Logements;
--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -1,14 +1,14 @@
 import Header from "../components/Header.jsx";
 import Banner from "../components/Banner.jsx";
 import bannerImage from "/src/assets/img-logo/IMG.png";
-import LogementsContainer from "../components/LogementsContainer.jsx";
+import Logement from "../components/Logements.jsx"
 import Footer from "../components/Footer.jsx";
 
 function Accueil() {
   return (
     <>
       <Banner image={bannerImage} text="Chez vous, partout et ailleurs" />
-      <LogementsContainer />
+      <Logement />
     </>
   );
 }
