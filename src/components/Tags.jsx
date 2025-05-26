14918e7a201bad24231d5bcc3f3efe2e426452a7
roobens landre <roobens971@gmail.com>
2025-05-26 20:30:08 +0200

    Erreur react dans le composant Tags :bug:

--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -1,7 +1,8 @@
 function Tags({tags}) {
   return (
+
     <div className="tags">
-     {tags.map((tag) => <span>{tag}</span>)}
+     {tags.map((tag) => <span key={tag}>{tag}</span>)}
     </div>
   );
 }
